b8989790aadb9dabbda387cd241444604ec2b96f
Julien leloup <jleloup@talend.com>
2016-08-23 10:02:49 +0200

    DEVOPS-894 - Update classes tests for the refactored module

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,19 +9,51 @@ describe 'cloudwatch' do
     it {should compile}
 
     it { should contain_class('cloudwatch') }
+    it { should contain_class('cloudwatch::install') }
+    it { should contain_class('cloudwatch::config') }
+    it { should contain_class('cloudwatch::params') }
     it { should contain_class('awscli') }
 
-    it {should contain_file('/opt/talend/cloudwatch/metrics.d').with({
-                                                                         'mode' => '0755',
-                                                                         'ensure' => 'directory'
-                                                                     })
+    it {should contain_file('/opt/cloudwatch-agent/metrics.d').with({
+            'mode'   => '0744',
+            'ensure' => 'directory',
+            'owner'  => 'cloudwatch-agent',
+        })
+    }
+
+    it {should contain_file('/opt/cloudwatch-agent/venv').with({
+            'mode'   => '0744',
+            'ensure' => 'directory',
+            'owner'  => 'cloudwatch-agent',
+        })
+    }
+
+    it {should contain_file('/var/log/cloudwatch-agent').with({
+            'mode'   => '0744',
+            'ensure' => 'directory',
+            'owner'  => 'cloudwatch-agent',
+        })
+    }
+
+    it {should contain_file('/opt/cloudwatch-agent/cloudwatch-agent.py').with({
+            'mode'   => '0744',
+            'ensure' => 'file',
+            'owner'  => 'cloudwatch-agent',
+        })
+
+    it {should contain_file('/opt/cloudwatch-agent/configuration.yaml').with({
+            'mode'   => '0744',
+            'ensure' => 'file',
+            'owner'  => 'cloudwatch-agent',
+        })
     }
 
     it { should contain_cron('cloudwatch_metrics').with({
-                                                            'command' => '/usr/local/bin/send_metrics',
-                                                            'user'    => 'root',
-                                                            'minute'  => '*/1',
-                                                        }) }
+            'command' => '/opt/cloudwatch-agent/venv/bin/python /opt/cloudwatch-agent/cloudwatch-agent.py >/dev/null 2>&1',
+            'user'    => 'cloudwatch-agent',
+            'minute'  => '*/1',
+        })
+    }
 
   end
 end
