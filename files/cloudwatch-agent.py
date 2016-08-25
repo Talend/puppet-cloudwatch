6afff5331eb52fdba3dfd89355d34f8c33d30f90
Julien leloup <jleloup@talend.com>
2016-08-25 14:16:50 +0200

    DEVOPS-894 - Get logging to work on main script.

--- files/cloudwatch-agent.py
+++ files/cloudwatch-agent.py
@@ -4,12 +4,13 @@
 """CloudWatch Agent
 
 Usage:
-  cloudwatch-agent.py [-d | --debug]
+  cloudwatch-agent.py [-d | --debug] -c <file> | --config <file>
   cloudwatch-agent.py (-h | --help)
 
 Options:
-  -d --debug    Set a more verbose logging.
-  -h --help     Show this screen.
+  -c <file> --config <file>   Absolute path to a YAML configuration file
+  -d        --debug           Set a more verbose logging.
+  -h        --help            Show this screen.
 
 """
 
@@ -18,16 +19,49 @@ import logging
 import logging.config
 import yaml
 
+# ---------
+# Functions
+# ---------
 
+
+def print_steps(func, level=logging.INFO):
+
+    def log_steps(*args, **kwargs):
+        logger.log(level, "BEGIN - {0}".format(func.__name__))
+        func(*args, **kwargs)
+        logger.log(level, "END - {0}".format(func.__name__))
+
+    return log_steps
+
+
+@print_steps
 def main(kwargs):
 
-    print("Loading configuration & initializing logging")
-    configuration = yaml.load("configuration.yaml")
+    # Match configuration with found scripts
+    # -> error log for every missing script
+    # -> execute all found scripts
+    logger.info("Get metric configuration")
 
-    logging.config.dictConfig(configuration['logging'])
+    metrics = configuration["metrics"]
 
-# --------------
+    logger.debug("Configured metrics : {0}".format(metrics))
+
+    # Use multiprocess to run scripts
+    # -> error log for failing scripts
+    # -> store values for all successful scripts
+
+    # Loop results to push them to CloudWatch using Boto
+
+
+# ---------
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    main(arguments)
+
+    with open(arguments["--config"]) as f:
+        configuration = yaml.load(f)
+
+        logging.config.dictConfig(configuration["logging"])
+        logger = logging.getLogger("cloudwatch-agent")
+
+        main(arguments, configuration)
