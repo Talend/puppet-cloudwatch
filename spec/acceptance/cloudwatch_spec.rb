bffa856d5182e3ecdd51e77e6fbdf842499c5872
Julien leloup <jleloup@talend.com>
2016-08-30 10:05:17 +0200

    DEVOPS-894 - Fix acceptance test

--- spec/acceptance/cloudwatch_spec.rb
+++ spec/acceptance/cloudwatch_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'cloudwatch' do
 
   describe user('cloudwatch-agent') do
-    if { should exists }
+    if { should exist }
   end
 
   describe file('/opt/cloudwatch-agent/cloudwatch-agent.py') do
