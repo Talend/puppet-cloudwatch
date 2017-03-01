7faa323a48466961bddce7995ed8d49734ec5d3c
Matthieu Debarre <mdebarre@talend.com>
2017-03-01 15:56:29 +0100

    DEVOPS-2037 memory and load monitoring for CI instances

---
+++ files/cloudwatch_agent/metrics.d/loadaverage.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Shell script by CloudWatch Agent to get the percentage metric that represent
+# loadaverage during last minute depending to the number of cpu cores.
+# The load average represents the average system load over a period of time.
+# It conventionally appears in the form of three numbers which represent the
+# system load during the last one-, five-, and fifteen-minute periods.
+# Copyright 2016 Talend
+
+function usage {
+    echo "$0
+
+    Shell script by CloudWatch Agent to get percentage metric.
+    the loadaverage for the last minute divised by the number of vcpu owned
+    by the instance.
+
+    Copyright 2017 Talend
+    "
+}
+
+COMMAND_BC=$(which bc)
+LOAD1=$(cat /proc/loadavg | cut -d ' ' -f 1)
+NB_VCPU=$(cat /proc/cpuinfo |grep processor |tail -n1| cut -d ':' -f 2)
+POURCENT=$(${COMMAND_BC} <<< "scale = 10; ${LOAD1} / (${NB_VCPU} + 1)")
+
+logger "CloudWatchMetric - loadaverage1min/NumberOfVCPU / ${POURCENT}"
+echo ${POURCENT}
